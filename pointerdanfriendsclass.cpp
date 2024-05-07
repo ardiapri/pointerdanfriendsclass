bcc91a5e28aec90676f79a6b5bab9a44fe1daeeb
ardiapri <ardiapri882@gmail.com>
2024-05-07 18:22:45 +0700

    int main

--- pointerdanfriendsclass.cpp
+++ pointerdanfriendsclass.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 class mahasiswa {
 public:
 	int nim;
@@ -7,3 +8,18 @@ public:
 		cout << "No Induk = " << nim << endl;
 	}
 };
+
+int main() {
+
+	mahasiswa mhs{ 1 }; // Object mhs
+	mhs.showNim(); // Member Access Operator
+
+	mahasiswa& refMhs = mhs; // Pointer References refMhs
+	refMhs.nim = 2; // Member Access Operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs; // Pointer Dereferences pMhs
+	pMhs->nim = 3; // Arrow Operator
+	pMhs->showNim();
+	return 0;
+}
